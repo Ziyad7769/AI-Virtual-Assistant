2f2d821a128bda7697cc3d0a4f76f2631ef3c9cb
Ziyad7769 <khanziyad286@gmail.com>
2025-06-18 19:39:13 +0530

    changes done in backend

--- backend/server.js
+++ backend/server.js
@@ -11,12 +11,24 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://virtual-assistant-fgs2.onrender.com",
+];
+
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
   })
 );
+
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/auth", authRouter);
